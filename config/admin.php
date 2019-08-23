0c7a25275d57b3ab01e903a65bced6f387f27b40
saurabh3111 <saurabhh3111@gmail.com>
2019-08-23 23:23:35 +0530

    first commit

---
+++ config/admin.php
@@ -0,0 +1,21 @@
+<?php
+
+ $servername="localhost";
+ $username="root";
+ $password="";
+ $dbname="admin";
+
+$conn=mysqli_connect($servername, $username, $password, $dbname); 
+
+ /*if($conn)
+ {
+    echo "connection successful";
+ }
+
+ else
+ 	{
+    
+     echo "connection failed";
+
+ 	}*/
+?>
---
+++ signup.php
@@ -0,0 +1,89 @@
+<?php
+ 
+  include("config/admin.php");
+
+if (isset($_POST['signup'])) 
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['pass'];
+    $repassword=$_POST['re_pass'];
+    // print_r($name);
+     //print_r($email);
+     //print_r($password);
+     //print_r($repassword);
+
+$insrtdata="INSERT INTO admindata(name,email,password,repassword) VALUES ('$name', '$email', '$password','$repassword')";
+    //print_r($insrtdata);
+    mysqli_select_db($conn,"admin.php");
+
+      
+     mysqli_query($conn,$insrtdata);
+}
+ 
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Userregistration</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form method="POST" class="register-form" id="register-form">
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="Your Name" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="pass" id="pass" placeholder="Password" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" required />
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+
+                            </div>
+                            
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+                        <a href="login.php" class="signup-image-link">I am already member</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
